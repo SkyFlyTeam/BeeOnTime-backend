0e6e0f507b8968810530372c0d727c2191722967
André Salerno <andresalerno22@gmail.com>
2025-03-26 18:32:12 -0300

    feat: Adicionando CRUD usuário | [SKF-32 e SKF-37] (#3)
    * feat: adicionando CRUD usuario
    
    * fix:criar-usuario; inserção de campos faltantes, ajustes nas chamadas das API's
    
    * hotfix: ajustando o modelo do banco
    
    * feat: adicionando crud de jornada
    
    * feat:criando o crud para jornada

---
+++ ms-usuario/src/main/java/com/fatec/ms_usuario/MsUsuarioApplication.java
@@ -0,0 +1,14 @@
+package com.fatec.ms_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MsUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsuarioApplication.class, args);
+	}
+
+}
