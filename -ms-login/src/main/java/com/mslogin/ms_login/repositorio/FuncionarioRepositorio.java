79758e1575ff789569071fe7952fdd314782e603
Eric Lourenco <ericloumendes@gmail.com>
2025-03-28 10:29:23 -0300

    hotfix: Ajustando organização de arquivos

--- -ms-login/src/main/java/com/mslogin/ms_login/repositorio/FuncionarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.mslogin.ms_login.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mslogin.ms_login.entidade.Funcionario;
-
-public interface FuncionarioRepositorio extends JpaRepository<Funcionario, Long> {
-
-}
