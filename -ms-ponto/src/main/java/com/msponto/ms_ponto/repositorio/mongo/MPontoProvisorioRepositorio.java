23c5bd03a4785cd807110ca627bd664b7ee64630
Eric Lourenço <77221715+ericloumendes@users.noreply.github.com>
2025-03-28 10:17:42 -0300

    Revert "Feat/conexao mongodb (#22)" (#25)
    This reverts commit 06ad76425ce960e239e4489c33132f7f0871755d.

--- -ms-ponto/src/main/java/com/msponto/ms_ponto/repositorio/mongo/MPontoProvisorioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.msponto.ms_ponto.repositorio.mongo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.msponto.ms_ponto.entidade.mongo.MPontoProvisorio;
-
-public interface MPontoProvisorioRepositorio extends MongoRepository<MPontoProvisorio, String>{
-    MPontoProvisorio findBySolicitacaoCod(Long solicitacaoCod);
-}
