2ef33bd92f66b96f491e5d1f1999c5307b5ebcda
karengoncalves8 <karengoncalves128@gmail.com>
2025-05-23 00:40:01 -0300

    feat: rota para relatorio de falta mensal

--- -ms-ponto/src/main/java/com/msponto/ms_ponto/controle/FaltaControle.java
+++ -ms-ponto/src/main/java/com/msponto/ms_ponto/controle/FaltaControle.java
@@ -1,8 +1,11 @@
 package com.msponto.ms_ponto.controle;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.msponto.ms_ponto.dto.UsuarioDTO;
 import com.msponto.ms_ponto.entidade.mysql.Falta;
+import com.msponto.ms_ponto.ms_clients.UsuarioClient;
 import com.msponto.ms_ponto.repositorio.mysql.FaltaRepositorio;
 
 
@@ -25,6 +30,9 @@ public class FaltaControle {
 
     @Autowired
     private FaltaRepositorio faltaRepositorio;
+    
+    @Autowired
+    private UsuarioClient usuarioClient;
 
     @GetMapping("/")
     public ResponseEntity<List<Falta>> getAllFaltas() {
@@ -43,6 +51,27 @@ public class FaltaControle {
         LocalDate converted_data = LocalDate.parse(data);
         Falta falta = faltaRepositorio.findByFaltaDiaAndUsuarioCod(converted_data, usuario_cod);
         return ResponseEntity.status(HttpStatus.OK).body(falta);
+    }
+
+    @GetMapping("/empresa/{empCod}/mes/{dia}")
+    public ResponseEntity<List<Falta>> getAllEmpresaFaltasByDate(@PathVariable Long empCod, @PathVariable LocalDate data) {
+        YearMonth anoMes = YearMonth.from(data);
+        LocalDate dataInicial = anoMes.atDay(1);
+        LocalDate dataFinal = anoMes.atEndOfMonth();
+        List<Falta> faltasMes = faltaRepositorio.findByFaltaDiaBetween(dataInicial, dataFinal);
+        List<UsuarioDTO> usuarios = usuarioClient.getAllUsuarios();
+        // Filtra só os usuários da empresa empCod
+        Set<Long> usuarioCodsDaEmpresa = usuarios.stream()
+            .filter(u -> empCod.equals(u.getEmpCod()))  
+            .map(UsuarioDTO::getUsuario_cod)  
+            .collect(Collectors.toSet());
+
+        // Filtra faltas para só as dos usuários da empresa
+        List<Falta> faltasDaEmpresa = faltasMes.stream()
+            .filter(f -> usuarioCodsDaEmpresa.contains(f.getUsuarioCod()))
+            .collect(Collectors.toList());
+
+        return ResponseEntity.ok(faltasDaEmpresa);
     }
 
     @PutMapping("/atualizar")
